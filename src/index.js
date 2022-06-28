0dde44b3bb04143402d26da1d656a4487f195247
DiegoRamirez <diego.israel.rm@gmail.com>
2022-06-28 16:38:20 -0500

    We correct a mistake

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app=express();
 const port=3000;
 
 app.get('/', (req, res) =>{
-  res.send("<body style="background: #E8F9FD "><h1 style="color: #2155CD;" >😎👌 Hola mundo 😎👌</h1><h2 style="color: #0AA1DD">💻 Desde Node.js y Express 💻</h2><h3 style="color: #79DAE8">🧑🏽‍💻 Atte: Diego Israel Ramirez Martinez 🧑🏽‍💻</h3></body>");
+  res.send('<body style="background: #E8F9FD "><h1 style="color: #2155CD;" >😎👌 Hola mundo 😎👌</h1><h2 style="color: #0AA1DD">💻 Desde Node.js y Express 💻</h2><h3 style="color: #79DAE8">🧑🏽‍💻 Atte: Diego Israel Ramirez Martinez 🧑🏽‍💻</h3></body>');
 })
 
 app.get('/nueva-ruta', (req, res) =>{
