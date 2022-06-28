b8ef94105ff2d1bbc86f2951c77da896e767a15f
DiegoRamirez <diego.israel.rm@gmail.com>
2022-06-28 15:52:06 -0500

    We change the text adding our name and the technology we use to build our app

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ const app=express();
 const port=3000;
 
 app.get('/', (req, res) =>{
-  res.send("Hola mi server en express");
+  res.send("Hola mundo desde NodeJs y Express \n Atte_ \nDiego Israel Ramirez Martinez");
+})
+
+app.get('/nueva-ruta', (req, res) =>{
+  res.send("Hola, soy una nueva ruta");
 })
 
 app.listen(port, ()=>{
